378715cfb84a23527889454dd466c4c6dd3f7dd3
rartstudio <58383924+rartstudio@users.noreply.github.com>
2020-10-02 11:44:47 +0700

    menambahkan file migrasi untuk table mentors, courses, chapters, lessons, my_courses, reviews, image_courses

--- database/migrations/2020_10_02_043753_create_reviews.php
+++ database/migrations/2020_10_02_043753_create_reviews.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateReviews extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->integer('rating')->default(1);
+            $table->longText('note')->nullable();
+            $table->unique(['course_id','user_id']);
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reviews');
     }
 }
