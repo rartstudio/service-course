5186e8371a50025e9ca458eea33c0e4ea1933960
rartstudio <58383924+rartstudio@users.noreply.github.com>
2020-10-04 17:37:42 +0700

    menambah routing untuk update mentor, menambah fungsi update pada mentor controller

--- app/Http/Controllers/MentorController.php
+++ app/Http/Controllers/MentorController.php
@@ -37,4 +37,49 @@ class MentorController extends Controller
 
         return response()->json(['status' => 'success', 'data' => $mentor]);
     }
+
+    public function update(Request $request, $id)
+    {
+        //rule schema
+        $rules = [
+            'name' => 'string',
+            'profile' => 'url',
+            'profession' => 'string',
+            'email' => 'email'
+        ];
+
+        //get all data
+        $data = $request->all();
+
+        //validasi
+        $validator= Validator::make($data,$rules);
+
+        //if any error
+        if($validator->fails()){
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ],404);
+        }
+
+        $mentor = Mentor::find($id);
+
+        if(!$mentor){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'mentor not found'
+            ],404);
+        }
+
+        //you may use the fill method to populate it with an array of attributes:
+        //using fillable mode in model
+        $mentor->fill($data);
+
+        //save data
+        $mentor->save();
+        return response()->json([
+            'status' => 'success',
+            'data' => $mentor
+        ]);
+    }
 }
